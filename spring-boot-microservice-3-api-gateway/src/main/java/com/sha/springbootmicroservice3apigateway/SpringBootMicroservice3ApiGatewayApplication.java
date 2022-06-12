dbfa28d0895128b59c42f848fe1613f01ad1b16d
fatih-batman <bfatih38@gmail.com>
2022-06-12 14:57:03 +0300

    .

---
+++ spring-boot-microservice-3-api-gateway/src/main/java/com/sha/springbootmicroservice3apigateway/SpringBootMicroservice3ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.sha.springbootmicroservice3apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//çalıştır postman test et
+@EnableFeignClients
+@SpringBootApplication
+public class SpringBootMicroservice3ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice3ApiGatewayApplication.class, args);
+	}
+
+}
